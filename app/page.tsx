18df5f49d80461a02b4e9e33b239a41cf8e99014
femi abimbola <femiabimbola6@gmail.com>
2024-07-11 15:39:25 -0700

    added the images and public files

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className=" text-white text-3xl "> Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+           <Image  src={"/assets/icons/logo-full.svg"} height={1000} width={1000} alt="logo-patient" className="mb-12 h-10 w-fit" />
+        </div>
+      </section>
     </div>
     
   );
