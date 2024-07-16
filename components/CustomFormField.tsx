cc971527a9358badc007bf19d93f1354d5d5493e
femi abimbola <femiabimbola6@gmail.com>
2024-07-16 14:57:27 -0700

    working on the render input

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,52 +1,63 @@
-"use client"
+"use client";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-  } from "@/components/ui/form"
-  
-import { Input } from "@/components/ui/input"
-import { Control } from "react-hook-form"
-import { FormFieldType } from "./forms/PatientForm"
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
 
 interface CustomProps {
-    control: Control<any>,
-    fieldType: FormFieldType,  //it helps you pick the error
-    name: string,
-    label?:string ,
-    placeholder?: string,
-    iconSrc?: string,
-    iconAlt?: string,
-    disabled?: boolean,
-    dateFormat?: string,
-    showTimeSelect?: boolean,
-    children?: React.ReactNode,
-    renderSkeleton?:(field:any) => React.ReactNode
+  control: Control<any>;
+  fieldType: FormFieldType; //it helps you pick the error
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
 }
 
-const RenderInput = {
+const RenderInput = ({ field, props}: {field:any; props: CustomProps}) => {
+  // return <Input type="text" placeholder="John Doe" />;
+  switch (props.fieldType) {
+    case FormFieldType.INPUT:
+      return( <div> </div>)
+      break;
   
-}
+    default:
+      break;
+  }
+};
 
-const CustomFormField = ({control, fieldType, name, label}: CustomProps) => {
+const CustomFormField = (props: CustomProps) => {
+  const { control, fieldType, name, label} = props;
   return (
     <FormField
-    // control={form.control}
-    control={control}
-    name={name}
-    render={({ field }) => (
-      <FormItem className="flex-1">
-      {fieldType !== FormFieldType.CHECKBOX && label && (
-        <FormLabel> {label} </FormLabel>
+      // control={form.control}
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label && (
+            <FormLabel> {label} </FormLabel>
+          )}
+          <RenderInput field={field} props={props}/>
+          <FormMessage className="shad-error" />
+        </FormItem>
       )}
-      </FormItem>
-    )}
-  />
-  )
-}
+    />
+  );
+};
 
-export default CustomFormField
+export default CustomFormField;
