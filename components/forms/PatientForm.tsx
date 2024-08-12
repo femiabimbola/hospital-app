60a7bb74f58d9ebfe633f7705f4524fcb93eaeb9
femi abimbola <femiabimbola6@gmail.com>
2024-08-11 23:28:14 -0700

    adding appwrite config

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -53,7 +53,7 @@ const PatientForm = () => {
       const userData = { name, email, phone };
 
       // const user = await createUser(userData);
-      // if (user) router.push("/patients/${user.$id}/register");
+      // if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
--- lib/zodValidation.ts
+++ lib/zodValidation.ts
@@ -5,6 +5,6 @@ export const PatientFormSchema = z.object({
     .string({ message: " Name can not be a digit" })
     .min(3, { message: "name must be 3 characters or more" })
     .max(50, "name must be at most 50 characters" ),
-  email: z.string().email('Ent er a vaild Email Address'),
+  email: z.string().email('Enter a vaild Email Address'),
   phone: z.string().refine((phone) => /^\+\d{10,15}$/.test(phone), "Invalid phone number"),
-});
+});  
