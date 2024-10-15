41d87e7af692d99994946839c8c1ca6ad2e1e88a
femi abimbola <femiabimbola6@gmail.com>
2024-10-14 23:21:19 -0700

    building the success page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import { ID } from "node-appwrite";
-import { database, DATABASE_ID, PATIENT_COLLECTION_ID } from "../appwrite.config";
+import { database, DATABASE_ID, PATIENT_COLLECTION_ID,APPOINTMENT_COLLECTION_ID, } from "../appwrite.config";
 import { parseStringify } from "../utils";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
@@ -12,11 +12,25 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
       ID.unique(),
       appointment
     );
-    
     return parseStringify(createAppointment);
-    
   } catch (error) {
     
   }
- 
 }
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await database.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the existing patient:",
+      error
+    );
+  }
+};
