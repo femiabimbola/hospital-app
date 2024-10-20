b65caa445aef95c6b35591c5b99b80697e9bc97c
femi abimbola <femiabimbola6@gmail.com>
2024-10-20 01:47:50 -0700

    updating he appointment|

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -3,6 +3,7 @@
 import { ID, Query } from "node-appwrite";
 import { database, DATABASE_ID, PATIENT_COLLECTION_ID,APPOINTMENT_COLLECTION_ID, } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 //3.10
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
@@ -37,3 +38,42 @@ export const getAppointment = async (appointmentId: string) => {
     );
   }
 };
+
+//  GET RECENT APPOINTMENTS
+export const getRecentAppointmentList = async () => {
+  try {
+    const appointments = await database.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")]
+    );
+      const initialCounts = {
+        scheduledCount: 0,
+        pendingCount: 0,
+        cancelledCount: 0,
+      };
+  
+      const counts = (appointments.documents as Appointment[]).reduce(
+        (acc, appointment) => {
+          if (appointment.status === "scheduled"){
+            acc.scheduledCount += 1
+          }else if (appointment.status === "pending"){
+            acc.pendingCount += 1
+          } else if ( appointment.status === "cancelled"){
+            acc.cancelledCount += 1;
+          } 
+          return acc;
+        },
+        initialCounts
+      );
+      const data = {
+        totalCount: appointments.total,
+        ...counts,
+        documents: appointments.documents,
+      };
+  
+      return parseStringify(data);
+  }catch (error) {
+    console.error("An error occurred while creating a new appointment:", error);
+  }
+} 
